277135bef795d2e9a1b40369a8de2d31fe8903f7
yulia <yuliia_yaresko@knu.ua>
2024-11-16 14:09:35 +0200

    Add project files.

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,16 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Hello World!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
